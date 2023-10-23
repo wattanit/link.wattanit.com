f0b6fef5fae6cda65cc6308fbf803fd21c4bab58
Wattanit Hotrakool <wattanit@wattanit.com>
2023-10-23 12:52:46 +0700

    Switch from URL path to search param
    Static hosting expects an HTML file that matches the URL path,
    making it unable to easily process URL path on a single page app.
    Simply switching to search param should just make the index.html
    processes all the parameters instead of looking for another file.

--- src/App.tsx
+++ src/App.tsx
@@ -7,35 +7,58 @@ function lookupUrl(path: string): string{
         path = path.slice(1);
     }
 
-    if (path in urls) {
+    let lookup_path = path.toLowerCase();
+
+    if (lookup_path in urls) {
         // @ts-ignore
-        return urls[path];
+        return urls[lookup_path];
     } else {
         return "Not found";
     }
 }
 
-function isDevMode(path: string): boolean{
-    return path === "?dev";
+type inputUrl = {
+    shortUrl: string,
+    isDev: boolean
+}
+
+function processUrl(url: string): inputUrl{
+    if (url[0] === "?"){
+        url = url.slice(1);
+    }
+
+    let path_items = url.split("&");
+
+    let isDevMode = false;
+    if (path_items.indexOf("dev") !== -1) {
+        isDevMode = true;
+
+        path_items.splice(path_items.indexOf("dev"),1);
+    }
+
+    return {
+        shortUrl: (path_items.length > 0)? path_items[0] : "",
+        isDev: isDevMode
+    };
 }
 
 function App() {
-    let isDev = isDevMode(window.location.search);
-    let redirectUrl = lookupUrl(window.location.pathname);
+    let urls = processUrl(window.location.search);
+    let redirectUrl = lookupUrl(urls.shortUrl);
 
     useEffect(() => {
         return ()=>{
             if (redirectUrl === "Not found") {
-                console.log("Short link not found: " + window.location.pathname);
+                console.log("Short link not found: " + urls.shortUrl);
                 console.log("Redirecting to https://wattanit.com");
-                if (!isDev){
+                if (!urls.isDev){
                     window.location.replace("https://wattanit.com");
                 }
             }
             else{
-                console.log("Short link found: " + window.location.pathname);
+                console.log("Short link found: " + urls.shortUrl);
                 console.log("Redirecting to " + redirectUrl);
-                if (!isDev){
+                if (!urls.isDev){
                     window.location.replace(redirectUrl);
                 }
             }
@@ -49,8 +72,8 @@ function App() {
             <span className={"Main"}>URL Shortener Service</span>
         </div>
         <p/>
-        {isDev && <div>Development mode: No redirection will occur</div>}
-        <div>Current link {window.location.pathname}</div>
+        {urls.isDev && <div>Development mode: No redirection will occur</div>}
+        <div>Current link {urls.shortUrl}</div>
         <div>Redirecting to {redirectUrl}</div>
     </div>
     );
